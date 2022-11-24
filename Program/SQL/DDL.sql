b8e1965b6890f75e31903b7083123a49b8589ef1
rlarkdals1202 <rlarkdals1202@gmail.com>
2022-11-24 14:59:51 +0900

    Database Systems Term Project 데이터베이스 테이블 생성

---
+++ Program/SQL/DDL.sql
@@ -0,0 +1,100 @@
+#데이터베이스 변경
+USE abandoned_animal_care;
+
+#보호소 테이블
+CREATE TABLE shelter
+(
+    shelter_id VARCHAR(500) NOT NULL PRIMARY KEY,
+    shelter_address VARCHAR(500),
+    FOREIGN KEY(shelter_address) REFERENCES shelter_address_book(shelter_address),
+    UNIQUE(shelter_address)
+);
+
+#보호소 주소록 테이블
+CREATE TABLE shelter_address_book
+(
+    shelter_address VARCHAR(500) NOT NULL PRIMARY KEY,
+    shelter_name VARCHAR(1000)
+);
+
+#동물 병원 테이블
+CREATE TABLE animal_hospital
+(
+    animal_hospital_id VARCHAR(500) NOT NULL PRIMARY KEY,
+    animal_hospital_address VARCHAR(500),
+    FOREIGN KEY(animal_hospital_address) REFERENCES animal_hospital_address_book(animal_hospital_address),
+    UNIQUE(animal_hospital_address)
+);
+
+#동물 병원 주소록 테이블
+CREATE TABLE animal_hospital_address_book
+(
+    animal_hospital_address VARCHAR(500) NOT NULL PRIMARY KEY,
+    animal_hospital_name VARCHAR(1000),
+    animal_hospital_contact VARCHAR(50),
+    animal_hospital_information TEXT
+);
+
+#협력 테이블
+CREATE TABLE cooperation
+(
+    shelter_id VARCHAR(500),
+    animal_hospital_id VARCHAR(500),
+    FOREIGN KEY(shelter_id) REFERENCES shelter(shelter_id),
+    FOREIGN KEY(animal_hospital_id) REFERENCES animal_hospital(animal_hospital_id)
+);
+
+#직원 테이블
+CREATE TABLE employee
+(
+    employee_id VARCHAR(100) NOT NULL PRIMARY KEY,
+    employee_passowrd VARCHAR(100),
+    employee_gender VARCHAR(10),
+    employee_birthday DATE,
+    employee_phone_number VARCHAR(50),
+    shelter_id VARCHAR(500) NOT NULL,
+    FOREIGN KEY(shelter_id) REFERENCES shelter(shelter_id)
+);
+
+#동물 테이블
+CREATE TABLE animal
+(
+    animal_id VARCHAR(100) NOT NULL PRIMARY KEY,
+    animal_sort VARCHAR(90),
+    animal_gender VARCHAR(10),
+    animal_age INTEGER,
+    employee_id VARCHAR(100),
+    animal_memo TEXT,
+    FOREIGN KEY(employee_id) REFERENCES employee(employee_id)
+);
+
+#동물 품종 정보 테이블
+CREATE TABLE animal_sort_information
+(
+    animal_sort VARCHAR(90),
+    animal_type VARCHAR(90)
+);
+
+#프로그램 개선사항 게시물 테이블
+CREATE TABLE program_improvements
+(
+    board_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    board_title VARCHAR(300),
+    board_category VARCHAR(100),
+    board_date DATE,
+    board_content MEDIUMTEXT,
+    employee_id VARCHAR(100),
+    FOREIGN KEY(employee_id) REFERENCES employee(employee_id)
+);
+
+#보호소 건의사항 게시물 테이블
+CREATE TABLE shelter_suggestions
+(
+    board_id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    board_title VARCHAR(300),
+    board_category VARCHAR(100),
+    board_date DATE,
+    board_content MEDIUMTEXT,
+    employee_id VARCHAR(100),
+    FOREIGN KEY(employee_id) REFERENCES employee(employee_id)
+);
